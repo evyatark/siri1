e84517a745c6b214d1a36154f0fa21d21c43c5ae
evyatark <evyatar@tikalk.com>
2018-05-31 16:01:49 +0300

    .

--- src/main/java/org/hasadna/bus/service/SiriConsumeServiceImpl.java
+++ src/main/java/org/hasadna/bus/service/SiriConsumeServiceImpl.java
@@ -185,6 +185,18 @@ public class SiriConsumeServiceImpl implements SiriConsumeService {
             logger.info("retrieveSiri");
             String content = retrieveSpecificLineAndStop(stopCode, previewInterval, lineRef, maxStopVisits);
 
+            // remove soap envelope (ugly)
+            final String prefix = "<?xml version='1.0' encoding='UTF-8'?><S:Envelope xmlns:S=\"http://schemas.xmlsoap.org/soap/envelope/\"><S:Body>";
+            final String suffix = "</S:Body></S:Envelope>";
+            if (content.startsWith(prefix)) {
+                content = content.substring(prefix.length());
+            }
+            if (content.endsWith(suffix)) {
+                content = content.substring(0,content.length()-suffix.length());
+            }
+            logger.info(content);
+
+            //unmarshall XML to object
             JAXBContext jaxbContext = JAXBContext.newInstance(GetStopMonitoringServiceResponse.class);
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
             StreamSource streamSource = new StreamSource(new StringReader(content));
